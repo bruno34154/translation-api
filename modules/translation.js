6e9bbdff7580839d3c8ecf621d42b78ea422163b
bruno34154 <www.brunomesenga@gmail.com>
2022-11-15 20:09:13 -0300

    adding lambda

--- modules/translation.js
+++ modules/translation.js
@@ -1,7 +1,28 @@
-const puppeteer = require("puppeteer");
+//const puppeteer = require("puppeteer");
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
+
+let options = {};
+
 
 async function translatetext(translate) {
-  const browser = await puppeteer.launch();
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+  const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
   const url = `https://translate.google.com/#view=home&op=translate&sl=auto&tl=${ translate.lang}&text=${encodeURIComponent(translate.content)}`;
 
